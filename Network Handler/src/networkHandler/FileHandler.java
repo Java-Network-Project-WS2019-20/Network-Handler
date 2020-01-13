311ddfce6675f2d4b583e8e9962638c275bd5fdb
skillstarjr <khalidbutt@live.de>
2020-01-13 16:22:47 +0100

    added javadoc + comments

--- Network Handler/src/networkHandler/FileHandler.java
+++ Network Handler/src/networkHandler/FileHandler.java
@@ -13,45 +13,77 @@ import java.io.IOException;
 import java.util.ArrayList;
 
 
+/**
+ * Class is used to handle the provided graphml file from the user. The main task of this class
+ * is to create an parsable document object which points to the provided graphml file from the user
+ * and also to parse the document for its nodes and edges, which are then saved into ArrayLists.
+ * @author Khalid Butt
+ * @author Sebastian Monok
+ */
+
 public class FileHandler {
 
+
+	/**
+	 * Points to the provided graphml file from the user.
+	 */
 	private File graphmlFile;
+
+	/**
+	 * Contains all nodes from the provided graphml file.
+	 */
 	private ArrayList<networkHandler.Node> nodeList;
+
+	/**
+	 * Contains all edges from the provided graphml file.
+	 */
 	private ArrayList<Edge> edgeList;
 
 	
 	public FileHandler() {
 
+		//Init
 		this.nodeList = new ArrayList<>();
 		this.edgeList = new ArrayList<>();
 		this.graphmlFile = null ;
 
 	}
 
+	/**
+	 * Specify the filepath of the graphml file.
+	 * @param filePath
+	 * Parameter is the filepath which is provided by the user of the graphml file
+	 */
 	public void setGraphmlFile(String filePath) {
 		this.graphmlFile = new File(filePath);
 	}
-	
+
+	/**
+	 * Return the ArrayList with the extracted nodes from provided the graphml file.
+	 * @return <code>ArrayList</code>
+	 */
 	public ArrayList<networkHandler.Node> getNodeList() {
 		return nodeList;
 	}
-	
+
+	/**
+	 * Return the ArrayList with the extracted edges from provided the graphml file.
+	 * @return <code>ArrayList</code>
+	 */
 	public ArrayList<Edge> getEdgeList() {
 		return edgeList;
 	}
 
 
 	/**
-	 * This methods creates a parsable document by using {@code DocumentBuilderFactory},
-	 * {@code DocumentBuilder} and {@code Document}. The parsable document is specified with the filepath of the
-	 * graphml file which has to be parsed.
-	 * @throws ParserConfigurationException
-	 * @throws IOException
-	 * @throws SAXException
+	 * Create a parsable document. The Document object is specified with the filepath of the
+	 * graphml file which is provided by the user. The graphml file is then passed to {@link FileHandler#graphmlParserEdges(Document)}
+	 * and {@link FileHandler#graphmlParserEdges(Document)} to parse the document for the nodes and edges.
 	 */
 
 	public void prepareParser(){
 
+		//Create a parsable document
 		DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
 		DocumentBuilder builder = null;
 		try {
@@ -70,19 +102,20 @@ public class FileHandler {
 			e.printStackTrace();
 		}
 
+		//Pass the parsable document to nodeParser and edgeParser method
 		graphmlParserNodes(document);
 		graphmlParserEdges(document);
-
-
 
 	}
 
 
 	/**
-	 * Parameter {@code Document} is parsed by "node" tag. The extracted nodes are saved in an ArrayList.
-	 * @param document
+	 * Parse Document object for nodes and save them into an ArrayList.
+	 * This is done by extracting all nodes into a {@link NodeList} by defining the tag element "node" as parameter in
+	 * the function {@link Document#getElementsByTagName(String)}.
+	 * The {@link NodeList} is then iterated for each node and saved into {@link FileHandler#nodeList}.
+	 * @param document The provided graphml by the user is passed to this method.
 	 */
-
 	public void graphmlParserNodes(Document document) {
 
 		// reading all nodes into nodesNL
@@ -101,10 +134,12 @@ public class FileHandler {
 
 
 	/**
-	 * {@code Document} is parsed by "edge" tag. The extracted edges are saved in an ArrayList.
-	 * @param document
+	 * Parse Document object for edges and saves them into an ArrayList.
+	 * This is done by extracting all edges into a {@link NodeList} by defining the tag element "edge" as parameter in
+	 * the function {@link Document#getElementsByTagName(String)}.
+	 * The {@link NodeList} is then iterated for each edge and saved into {@link FileHandler#edgeList}.
+	 * @param document The provided graphml by the user is passed to this method.
 	 */
-	
 	public void graphmlParserEdges(Document document) {
 		// reading all edges into edgesNL
 				NodeList edgesNL = document.getElementsByTagName("edge");
