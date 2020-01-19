a7bf5516afbae8501c3cdc145a4ed4c9ad13fc1f
Fabian <fabian.grun@gmx.de>
2020-01-19 16:31:31 +0100

    implemented class BetweennessCentralityMeasure
    the class takes a graph, a set of shortest paths and a node id as input.
    it calculates the BCM for the given node.

--- Network Handler/src/networkHandler/BetweennessCentralityMeasure.java
+++ Network Handler/src/networkHandler/BetweennessCentralityMeasure.java
@@ -1,19 +1,44 @@
 package networkHandler;
 
-import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.TreeSet;
 
 public class BetweennessCentralityMeasure extends GraphProperty<Double>{
 	//	Attribute
-	private	ArrayList<Path>	shortestPaths;
+	private	TreeSet<Path>	shortestPaths;
+	private int				nodeId;
 	
 	//	Constructor
-	public	BetweennessCentralityMeasure(Graph graph, ArrayList<Path> shortestPaths) {
+	public			BetweennessCentralityMeasure(Graph graph, TreeSet<Path> shortestPaths, int nodeId) {
 		super(graph);
-		this.shortestPaths = shortestPaths;
+		this.shortestPaths	= shortestPaths;
+		this.value			= 0.0;
+		this.nodeId			= nodeId;
 	}
 	
 	//	implementation of calculate from GraphProperty Superclass
 	public	void	calculate() {
 		
+		double[][] countsOfAllPaths = new double[this.graph.getNodeCount()][this.graph.getNodeCount()];
+		double[][] countsOfPathsContainingNode = new double[this.graph.getNodeCount()][this.graph.getNodeCount()];
+		
+		Iterator<Path> iterator = this.shortestPaths.iterator();
+		while(iterator.hasNext()) {
+			Path path = iterator.next();
+			countsOfAllPaths[path.getOriginNode()][path.getDestinationNode()]++;
+			if(path.contains(nodeId)) {
+				countsOfPathsContainingNode[path.getOriginNode()][path.getDestinationNode()]++;
+			}
+		}
+		
+		for(int i = 0; i < this.graph.getNodeCount(); i++) {
+			if(i != nodeId) {
+				for(int j = i + 1; j < this.graph.getNodeCount(); j++) {
+					if(j != nodeId) {
+						this.value = this.value + (countsOfPathsContainingNode[i][j] / countsOfAllPaths[i][j]);
+					}
+				}
+			}
+		}
 	}
 }
