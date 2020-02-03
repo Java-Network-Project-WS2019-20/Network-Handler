4f753ad4c18349a201b0376dd9a248834faaa06e
KGoroll <Krzysztof.goroll@gmail.com>
2020-02-03 21:52:42 +0100

    Cleaned Code

--- Network Handler/src/networkHandler/Connectivity.java
+++ Network Handler/src/networkHandler/Connectivity.java
@@ -2,80 +2,81 @@ package networkHandler;
 
 import java.util.LinkedList;
 
-public class Connectivity implements GraphProperty<Boolean>{
-//	Attributes
-	private	Graph	graph;
-	private	Boolean	connectivityValue;
-	
-//	Constructor
-	public	Connectivity(Graph graph) {
-		this.graph	= graph;
+/**
+ * Calculates whether a {@link Graph} is connected or not. Using the Depth First
+ * Search Algorithm
+ * 
+ * @author Krzysztof
+ */
+
+public class Connectivity implements GraphProperty<Boolean> {
+
+	private Graph graph;
+	private Boolean connectivityValue;
+
+	public Connectivity(Graph graph) {
+		this.graph = graph;
 	}
-	
-//	implementation of getValue method
-	public	Boolean	getValue() {
+
+	public Boolean getValue() {
 		return this.connectivityValue;
 	}
-	
-	
-//	alternative getter method
-	public Boolean	getConnectivity() {
+
+	public Boolean getConnectivity() {
 		return this.getValue();
 	}
-	
-//	implementation of calculate method from GraphProperty Superclass
-	// Method to calculate, whether a undirected Graph is connected
-	// Not minding the weight of the edges
+
+	@SuppressWarnings("unchecked")
 	public void run() {
 
-		// A linked list of all adjacent Nodes for each Node
+		// A array of linked lists of all adjacent Nodes for each Node
 		// Size of array is the number of Nodes
-		LinkedList<Integer>[] adjListArray = new LinkedList[this.graph.getNodeCount()];
-		
-		// Create a new list for each Node
-		// so that adjacent nodes can be stored
+		LinkedList<Integer>[] adjacentListArray = new LinkedList[this.graph.getNodeCount()];
 		for (int i = 0; i < this.graph.getNodeCount(); i++) {
-		adjListArray[i] = new LinkedList<Integer>();
+			adjacentListArray[i] = new LinkedList<Integer>();
 		}
 		// Adds all edges to the nodes
-		// Since graph is undirected, add an edge from source to target
-		// as well as target to source
+		// Since graph is undirected, add an edge from source to target as well as
+		// target to source
 		for (Edge e : this.graph.getEdgeList()) {
-			adjListArray[e.getSource()].add(e.getTarget());
-			adjListArray[e.getTarget()].add(e.getSource());
+			adjacentListArray[e.getSource()].add(e.getTarget());
+			adjacentListArray[e.getTarget()].add(e.getSource());
 		}
-		
-		// Mark all Nodes as not visited
 		boolean[] visited = new boolean[this.graph.getNodeCount()];
-		// Calculate all reachable Nodes from Node 1
-		// If one Node is not reachable, it means the graph
-		// Is not connected
-		dfsVisit(0,visited,adjListArray);
+		// Calculate all reachable Nodes
+		// If one Node is not reachable it means the graph is not connected
+		doVisitNodes(0, visited, adjacentListArray);
 		this.connectivityValue = true;
-		for(int v = 0; v < this.graph.getNodeCount(); v++) {
-			if(!visited[v])
-				this.connectivityValue = false;				
+		for (int v = 0; v < this.graph.getNodeCount(); v++) {
+			if (!visited[v])
+				this.connectivityValue = false;
 		}
-
 	}
-	
-	// Method to calculate, which nodes are reachable
-	// From a certain node v
-	// Using the Depth First Search Algorithm
-	public boolean[] dfsVisit(int v, boolean[] visited, LinkedList<Integer>[] adjListArray) {
-		// Mark the current node as visited
+
+	/**
+	 * Method to calculate, which nodes are reachable from a certain node v
+	 * 
+	 * @param v                 start node
+	 * @param visited           all visited nodes
+	 * @param adjacentListArray A array of linked lists of all adjacent Nodes for
+	 *                          each Node
+	 * @return visited nodes afterwards
+	 */
+	public boolean[] doVisitNodes(int v, boolean[] visited, LinkedList<Integer>[] adjacentListArray) {
 		visited[v] = true;
-		// repeat for all the nodes
-		// adjacent to this node
-		for (int x : adjListArray[v]) {
-			if(!visited[x]) dfsVisit(x,visited,adjListArray);
+		// repeat for all the nodes adjacent to this node
+		for (int x : adjacentListArray[v]) {
+			if (!visited[x])
+				doVisitNodes(x, visited, adjacentListArray);
 		}
 		return visited;
 	}
 
-	public	void	printToConsole() {
+	public void printToConsole() {
 		System.out.print("The graph is ");
-		if(!getValue()) {System.out.print("not ");}
+		if (!getValue()) {
+			System.out.print("not ");
+		}
 		System.out.print("connected.\n");
 	}
 }
--- Network Handler/src/networkHandler/Diameter.java
+++ Network Handler/src/networkHandler/Diameter.java
@@ -1,44 +1,40 @@
 package networkHandler;
 
-
-import java.util.TreeSet;
-
+/**
+ * Calculates the Diameter of a {@link Graph}
+ * 
+ * @author Krzysztof
+ */
 public class Diameter implements GraphProperty<Double> {
-	//	Attributes
-	private	Double				diameterValue;
-	private	ShortestPathList	shortestPathList;
-	private	Connectivity		connectivity;
-	
-	//	Constructor
-	public			Diameter(ShortestPathList shortestPathList, Connectivity connectivity) {
-		this.shortestPathList	= shortestPathList;
-		this.connectivity		= connectivity;
+
+	private Double diameterValue;
+	private ShortestPathList shortestPathList;
+	private Connectivity connectivity;
+
+	public Diameter(ShortestPathList shortestPathList, Connectivity connectivity) {
+		this.shortestPathList = shortestPathList;
+		this.connectivity = connectivity;
 	}
-	
-	public	Double	getValue() {
+
+	public Double getValue() {
 		return this.diameterValue;
 	}
-	
-	//	alternative getter method
-	public	Double	getDiameter() {
-		return this.getValue();
-	}
-	
-	//	implementation of calculate method from GraphProperty Superclass
-	public	void	run() {
-		if(connectivity.getValue()) {
+
+	public void run() {
+		if (connectivity.getValue()) {
 			this.diameterValue = 0.0;
-			this.shortestPathList.getValue().forEach(path ->	{
-				if(path.getLength() > this.diameterValue) {
+			// check which shortest path is the biggest resp. longest
+			this.shortestPathList.getValue().forEach(path -> {
+				if (path.getLength() > this.diameterValue) {
 					this.diameterValue = path.getLength();
 				}
 			});
-		}else {
+		} else {
 			this.diameterValue = Double.POSITIVE_INFINITY;
 		}
 	}
-	
-	public	void	printToConsole() {
-		System.out.print("The Diameter of the graph is " + getValue().toString() + ".\n"); 
+
+	public void printToConsole() {
+		System.out.print("The Diameter of the graph is " + getValue().toString() + ".\n");
 	}
 }
