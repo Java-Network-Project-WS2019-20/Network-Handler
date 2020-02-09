6e80bffd85b9698ecb69e52d84efc46240a4708c
Fabian <fabian.grun@gmx.de>
2020-02-09 08:21:00 +0100

    cleanup + small javadoc edit

--- Network Handler/src/networkHandler/Node.java
+++ Network Handler/src/networkHandler/Node.java
@@ -4,30 +4,19 @@ import org.apache.log4j.LogManager;
 import org.apache.log4j.Logger;
 
 	/**
- 	* This class represents a node
+ 	* This class represents a node in a {@link Graph}.
  	*/
 
 public class Node {
-
-
-
-
-	/**
-	 * A node is identified with an id
-	 */
 	private int id;
 	private final Logger mylog = LogManager.getLogger(Node.class);
-
-
 
 	public Node(int id) {
 		this.id = id;
 	}
 
 	public int getID() { return id; }
-	public void setID(int id) { this.id = id; }
-	
-	
+
 	@Override
 	public String toString() {
 		StringBuilder sb = new StringBuilder();
@@ -35,11 +24,8 @@ public class Node {
 		return sb.toString();
 	}
 	
-	public	void	printToConsole() {
-
-
+	public void printToConsole() {
 		mylog.info(this.toString());
-//		System.out.print(this.toString()+ "\n");
 	}
 }
 
