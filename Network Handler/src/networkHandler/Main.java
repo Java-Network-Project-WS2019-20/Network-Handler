01afd585ba6094a3d57d5693c50f7576129af1cd
Fabian <fabian.grun@gmx.de>
2020-02-09 11:56:01 +0100

    added javadoc to Main class

--- Network Handler/src/networkHandler/Main.java
+++ Network Handler/src/networkHandler/Main.java
@@ -3,17 +3,31 @@ package networkHandler;
 import org.apache.log4j.LogManager;
 import org.apache.log4j.Logger;
 
+/**
+ * <p> The Main class is responsible for correct initialization of all other classes in the correct Order.
+ * <p> It creates instances of {@link CommandLineReader}, {@link GraphmlReader}, {@link GraphHandler}, {@link CommandLineWriter} and {@link GraphmlWriter}
+ * and is responsible for communication and forwarding between those classes.
+ * @author Fabian Grun
+ *
+ */
 public class Main {
 
 	private static final Logger mylog = LogManager.getLogger(GraphHandler.class);
 
+	/**
+	 * <p> The main method creates an instance of {@link CommandLineReader}, forwards the given arguments to it and calls its parsing method.
+	 * <p> After parsing the arguments it reads the necessary flags, calls the {@link GraphmlReader} if necessary and forwards the results to an instance of {@link GraphHandler}.
+	 * <p> After completing the calculations in {@link GraphHandler}, {@link GraphmlWriter} and {@link CommandLineWriter} are created called as instructed by the results of the {@link CommandLineReader}.
+	 * @param args
+	 * @throws NoArgumentException
+	 */
 	public static void main(String[] args) throws NoArgumentException {
 		try {
 			if (args.length > 0) {
 				CommandLineReader commandLineReader = new CommandLineReader(args);
 				commandLineReader.doParseCommandLineArguments();	
 				if(commandLineReader.getFlagReadFile()) {
-					GraphReader	graphReader = new GraphReader();
+					GraphmlReader	graphReader = new GraphmlReader();
 					graphReader.setGraphmlFile(commandLineReader.getInputFileName());
 					graphReader.prepareParser();
 					if (graphReader.isParseSuccessful()) {
@@ -22,7 +36,7 @@ public class Main {
 						Thread graphWriterThread = new Thread();
 						Thread commandLineWriterThread = new Thread();
 						if (commandLineReader.getFlagCreateOutputFile()) {
-							GraphWriter graphWriter = new GraphWriter(commandLineReader.getInputFileName(), commandLineReader.getOutputFileName(), graphHandler);
+							GraphmlWriter graphWriter = new GraphmlWriter(commandLineReader.getInputFileName(), commandLineReader.getOutputFileName(), graphHandler);
 							graphWriter.doCheckIfFileExists();
 							graphWriterThread = new Thread(graphWriter, "File Creation Thread");
 							graphWriterThread.start();
