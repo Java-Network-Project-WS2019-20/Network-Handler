e5cb2134491ff6fa214433448627a8d41cc7e999
Fabian <fabian.grun@gmx.de>
2020-02-09 06:37:16 +0100

    small edits to javadoc

--- Network Handler/src/networkHandler/GraphProperty.java
+++ Network Handler/src/networkHandler/GraphProperty.java
@@ -1,10 +1,11 @@
 package networkHandler;
 
 /**
- * Interface for calculations of properties of a {@link Graph}. It defines methods necessary for uniform calculation
+ * <p>Interface for calculations of properties of a {@link Graph}. It defines methods necessary for uniform calculation
  * and returning of results of different properties. 
- * The extension of the {@link Runnable} interface enables parallel calculations of multiple GraphProperty's via {@link Thread}s.
- * The class uses a generic type to enable use for different types of values as calculation results.
+ * <p>The extension of the {@link Runnable} interface enables parallel calculation of multiple GraphProperty's via {@link Thread}s.
+ * <p>The class uses a generic type to enable use for different types of values as calculation results.
+ * This also ensures, that an appropriate type is chosen for each implementing class, since each property requires a value.
  * @author Fabian Grun
  * @param <T> The type of the value(s) calculated by the implementing classes.
  * @see BetweennessCentralityMeasure
@@ -22,12 +23,12 @@ public interface GraphProperty<T> extends Runnable {
 	public T getValue();
 	
 	/**
-	 * Method responsible for the actual calculation of the properties.
+	 * Method responsible for the actual calculation of the property value(s).
 	 */
 	public abstract void run();
 
 	/**
-	 * Method for returning the results as a message to the console.
+	 * Method for printing the calculated result(s) as a message to the console.
 	 */
 	public void printToConsole();
 }
