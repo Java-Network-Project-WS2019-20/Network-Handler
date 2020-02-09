13b35b9147bc5888cda48a68c2bbef8872ef54f8
Fabian <fabian.grun@gmx.de>
2020-02-09 09:16:29 +0100

    javadoc + cleanup in graphhandler

--- Network Handler/src/networkHandler/GraphHandler.java
+++ Network Handler/src/networkHandler/GraphHandler.java
@@ -6,13 +6,20 @@ import java.util.ArrayList;
 import java.util.TreeSet;
 
 /**
- * Manages all the calculations. What should be calculated is determined by a
- * flag from the {@link CommandLineReader}. Also manages the print outs of the
- * calculations to the console.
+ * <p>This class is responsible for managing all from {@link GraphProperty} derived classes and initiates their calculation on a {@link Graph}, which is handled in this class as well.
+ * <p>It is also responsible for forwarding calls to the printing methods of the GraphProperties and calls to their value getters.
+ * <p>To determine the necessary calculations it checks the flags provided by a {@link CommandLineReader} accordingly.
  * 
  * @author Fabian Grun
  * @author Khalid Butt
- */
+ * @see BetweennessCentralityMeasure
+ * @see BetweennessCentralityMeasureList
+ * @see Connectivity
+ * @see Diameter
+ * @see GraphProperty
+ * @see MinimumSpanningTree
+ * @see ShortestPathList
+ */ 
 public class GraphHandler {
 	
 	private Graph graph;
@@ -29,7 +36,12 @@ public class GraphHandler {
 		this.commandLineReader = commandLineReader;
 	}
 
-	// TODO: error handling/logging
+	/**
+	 * <p>This method is responsible for handling all calculations.
+	 * <p>First it checks the flags provided by the commandLineReader and determines, which calculations are expected and also which might be dependent on others.
+	 * <p>In case a flag for file creation is set, all properties are calculated, otherwise only those specified by the printing flags of the {@link CommandLineReader}.
+	 * <p>Each {@link GraphProperty} is then calculated in its own {@link Thread} for efficiency, while making sure, dependencies are finished with their calculations before continuing to dependents.
+	 */
 	public void runCalculations() {
 		Thread connectivityThread = new Thread();
 		Thread shortestPathThread = new Thread();
@@ -41,135 +53,91 @@ public class GraphHandler {
 			this.connectivity = new Connectivity(this.graph);
 			connectivityThread = new Thread(this.connectivity, "Connectivity Calculation");
 			connectivityThread.start();
-
 			if (commandLineReader.getFlagShortestPathsTwoNodes()) {
-				this.shortestPathList = new ShortestPathList(this.graph,
-						commandLineReader.getFlagShortestPathsNoDuplicates(), true, commandLineReader.getSpIDone(),
-						commandLineReader.getSpIDtwo());
+				this.shortestPathList = new ShortestPathList(this.graph, commandLineReader.getFlagShortestPathsNoDuplicates(), true, commandLineReader.getShortestPathNodeID1(), commandLineReader.getShortestPathNodeID2());
 			} else {
-				this.shortestPathList = new ShortestPathList(this.graph,
-						commandLineReader.getFlagShortestPathsNoDuplicates());
+				this.shortestPathList = new ShortestPathList(this.graph, commandLineReader.getFlagShortestPathsNoDuplicates());
 			}
 			shortestPathThread = new Thread(this.shortestPathList, "Shortest Paths Calculation");
 			shortestPathThread.start();
-
 			try {
 				shortestPathThread.join();
 				connectivityThread.join();
 			} catch (InterruptedException e) {
-				// TODO Auto-generated catch block
-
 				mylog.error("Something went wrong: " +e.getMessage());
-
-//				e.printStackTrace();
 			}
-
 			this.minimumSpanningTree = new MinimumSpanningTree(this.graph, this.connectivity);
 			minimumSpanningTreeThread = new Thread(this.minimumSpanningTree, "Minimum Spanning Tree Calculation");
 			minimumSpanningTreeThread.start();
-
-			if (commandLineReader.getFlagBCMSingle()) {
-				this.betweennessCentralityMeasureList = new BetweennessCentralityMeasureList(this.graph,
-						this.shortestPathList, true, commandLineReader.getBcmNodeID());
+			if (commandLineReader.getFlagBetweennessCentralityMeasureSingle()) {
+				this.betweennessCentralityMeasureList = new BetweennessCentralityMeasureList(this.graph, this.shortestPathList, true, commandLineReader.getbetweennessCentralityMeasureNodeID());
 			} else {
-				this.betweennessCentralityMeasureList = new BetweennessCentralityMeasureList(this.graph,
-						this.shortestPathList);
+				this.betweennessCentralityMeasureList = new BetweennessCentralityMeasureList(this.graph, this.shortestPathList);
 			}
-			betweennessCentralityMeasureThread = new Thread(this.betweennessCentralityMeasureList,
-					"Betweenness Centrality Measure Calculation");
+			betweennessCentralityMeasureThread = new Thread(this.betweennessCentralityMeasureList, "Betweenness Centrality Measure Calculation");
 			betweennessCentralityMeasureThread.start();
-
 			this.diameter = new Diameter(this.shortestPathList, this.connectivity);
 			diameterThread = new Thread(this.diameter, "Diameter Calculation");
 			diameterThread.start();
-
 		} else {
-			// calculate connectivity?
-			if (commandLineReader.getFlagConnectivity() || commandLineReader.getFlagDiameter()
-					|| commandLineReader.getFlagMinimumSpanningTree()) {
+			if (commandLineReader.getFlagConnectivity() || commandLineReader.getFlagDiameter() || commandLineReader.getFlagMinimumSpanningTree()) {
 				this.connectivity = new Connectivity(this.graph);
 				connectivityThread = new Thread(this.connectivity, "Connectivity Calculation");
 				connectivityThread.start();
 			}
-
-			// calculate single/all shortest paths?
-			if (commandLineReader.getFlagShortestPathsAll() || commandLineReader.getFlagDiameter()
-					|| commandLineReader.getFlagBCMSingle() || commandLineReader.getFlagBCMAll()) {
+			if (commandLineReader.getFlagShortestPathsAll() || commandLineReader.getFlagDiameter() || commandLineReader.getFlagBetweennessCentralityMeasureSingle() || commandLineReader.getFlagBetweennessCentralityMeasureAll()) {
 				if (commandLineReader.getFlagShortestPathsTwoNodes()) {
-					this.shortestPathList = new ShortestPathList(this.graph,
-							commandLineReader.getFlagShortestPathsNoDuplicates(), true, commandLineReader.getSpIDone(),
-							commandLineReader.getSpIDtwo());
+					this.shortestPathList = new ShortestPathList(this.graph, commandLineReader.getFlagShortestPathsNoDuplicates(), true, commandLineReader.getShortestPathNodeID1(), commandLineReader.getShortestPathNodeID2());
 				} else {
-					this.shortestPathList = new ShortestPathList(this.graph,
-							commandLineReader.getFlagShortestPathsNoDuplicates());
+					this.shortestPathList = new ShortestPathList(this.graph, commandLineReader.getFlagShortestPathsNoDuplicates());
 				}
 				shortestPathThread = new Thread(this.shortestPathList, "Shortest Paths Calculation");
 				shortestPathThread.start();
 			} else {
 				if (commandLineReader.getFlagShortestPathsTwoNodes()) {
-					this.shortestPathList = new ShortestPathList(this.graph,
-							commandLineReader.getFlagShortestPathsNoDuplicates(), false, commandLineReader.getSpIDone(),
-							commandLineReader.getSpIDtwo());
+					this.shortestPathList = new ShortestPathList(this.graph, commandLineReader.getFlagShortestPathsNoDuplicates(), false, commandLineReader.getShortestPathNodeID1(), commandLineReader.getShortestPathNodeID2());
 					shortestPathThread = new Thread(this.shortestPathList, "Shortest Paths Calculation");
 					shortestPathThread.start();
 				}
 			}
-
-			// calculate minimum spanning tree?
 			if (commandLineReader.getFlagMinimumSpanningTree()) {
 				try {
 					connectivityThread.join();
 				} catch (InterruptedException e) {
-					// TODO Auto-generated catch block
 					mylog.error("Something went wrong: " +e.getMessage());
-
-//					e.printStackTrace();
 				}
 				minimumSpanningTree = new MinimumSpanningTree(this.graph, this.connectivity);
 				minimumSpanningTreeThread = new Thread(minimumSpanningTree, "Minimum Spanning Tree Calculation");
 				minimumSpanningTreeThread.start();
 			}
-			// calculate diameter?
 			if (commandLineReader.getFlagDiameter()) {
 				try {
 					shortestPathThread.join();
 					connectivityThread.join();
 				} catch (InterruptedException e) {
-					// TODO Auto-generated catch block
 					mylog.error("Something went wrong: " +e.getMessage());
 
-//					e.printStackTrace();
 				}
-
 				this.diameter = new Diameter(this.shortestPathList, this.connectivity);
 				diameterThread = new Thread(this.diameter, "Diameter Calculation");
 				diameterThread.start();
 			}
-
-			// calculate single/all betweenness centrality measure(s)?
-			if (commandLineReader.getFlagBCMSingle() || commandLineReader.getFlagBCMAll()) {
+			if (commandLineReader.getFlagBetweennessCentralityMeasureSingle() || commandLineReader.getFlagBetweennessCentralityMeasureAll()) {
 				try {
 					shortestPathThread.join();
 				} catch (InterruptedException e) {
-					// TODO Auto-generated catch block
-//					e.printStackTrace();
 					mylog.error("Something went wrong: " +e.getMessage());
-
 				}
-				if (commandLineReader.getFlagBCMAll() && commandLineReader.getFlagBCMSingle()) {
-					this.betweennessCentralityMeasureList = new BetweennessCentralityMeasureList(this.graph,
-							this.shortestPathList, true, commandLineReader.getBcmNodeID());
+				if (commandLineReader.getFlagBetweennessCentralityMeasureAll() && commandLineReader.getFlagBetweennessCentralityMeasureSingle()) {
+					this.betweennessCentralityMeasureList = new BetweennessCentralityMeasureList(this.graph, this.shortestPathList, true, commandLineReader.getbetweennessCentralityMeasureNodeID());
 				} else {
-					if (commandLineReader.getFlagBCMAll()) {
-						this.betweennessCentralityMeasureList = new BetweennessCentralityMeasureList(this.graph,
-								this.shortestPathList);
+					if (commandLineReader.getFlagBetweennessCentralityMeasureAll()) {
+						this.betweennessCentralityMeasureList = new BetweennessCentralityMeasureList(this.graph, this.shortestPathList);
 					} else {
-						this.betweennessCentralityMeasureList = new BetweennessCentralityMeasureList(this.graph,
-								this.shortestPathList, false, commandLineReader.getBcmNodeID());
+						this.betweennessCentralityMeasureList = new BetweennessCentralityMeasureList(this.graph, this.shortestPathList, false, commandLineReader.getbetweennessCentralityMeasureNodeID());
 					}
 				}
-				betweennessCentralityMeasureThread = new Thread(this.betweennessCentralityMeasureList,
-						"Betweenness Centrality Measure Calculation");
+				betweennessCentralityMeasureThread = new Thread(this.betweennessCentralityMeasureList, "Betweenness Centrality Measure Calculation");
 				betweennessCentralityMeasureThread.start();
 			}
 		}
@@ -180,12 +148,8 @@ public class GraphHandler {
 			diameterThread.join();
 			betweennessCentralityMeasureThread.join();
 		} catch (InterruptedException e) {
-			// TODO Auto-generated catch block
 			mylog.error("Something went wrong: " +e.getMessage());
-
-//			e.printStackTrace();
 		}
-
 	}
 
 	public Graph getGraph() {
